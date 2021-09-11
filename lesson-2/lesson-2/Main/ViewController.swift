84f9c43e3266400ede9127e889f9e2a454c1dc97
LACKY5533 <33594361+LACKY5533@users.noreply.github.com>
2021-09-11 23:51:22 +0300

    Add files via upload

---
+++ lesson-2/lesson-2/Main/ViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ViewController.swift
+//  lesson-1
+//
+//  Created by LACKY on 06.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var ScrollView: UIScrollView!
+    @IBOutlet private weak var LoginText: UITextField!
+    @IBOutlet private weak var PasswordText: UITextField!
+    @IBOutlet private weak var ChangeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addGesture()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.addObservers()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.removeObservers()
+    }
+    
+    private func addGesture () {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(handleGesture))
+        self.ScrollView.addGestureRecognizer(gesture)
+    }
+    
+    @objc private func handleGesture() {
+        self.ScrollView.endEditing(true)
+    }
+    
+    private func addObservers() {
+        NotificationCenter.default
+            .addObserver(self,
+                         selector: #selector(handleKeyboardWillShow),
+                         name: UIResponder.keyboardWillShowNotification,
+                         object: nil)
+        
+        NotificationCenter.default
+            .addObserver(self,
+                         selector: #selector(handleKeyboardWillHide),
+                         name: UIResponder.keyboardWillShowNotification,
+                         object: nil)
+    }
+    
+    private func removeObservers() {
+        NotificationCenter.default
+            .removeObserver(self,
+                            name: UIResponder.keyboardWillShowNotification,
+                            object: nil)
+        
+        NotificationCenter.default
+            .removeObserver(self,
+                            name: UIResponder.keyboardWillShowNotification,
+                            object: nil)
+    }
+    
+    @objc private func handleKeyboardWillShow() {
+        self.ScrollView.contentInset.bottom = 120
+    }
+    
+    @objc private func handleKeyboardWillHide() {
+        self.ScrollView.contentInset.bottom = 0
+    }
+    
+    private func checkPassword() {
+        let login = self.LoginText.text
+        let password = self.PasswordText.text
+        
+        if login == "admin" && password == "admin" {
+            self.showAdminScene()
+        }else {
+            self.showAllert()
+        }
+    }
+    
+    private func showAdminScene() {
+        let storyBoard = UIStoryboard(name: "DebugMenu", bundle: nil)
+        let viewController = storyBoard.instantiateInitialViewController()
+        if let viewController = viewController as? DebugViewController {
+            self.present(viewController, animated: true)
+        }
+    }
+    
+    private func showAllert() {
+        let allertController = UIAlertController(
+            title: "Ошибка",
+            message: "Введены неверные данные!",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "ОК",
+            style: .cancel)
+        
+        allertController.addAction(action)
+        self.present(allertController, animated: true)
+    }
+
+    @IBAction private func onButtonPressed(_ sender: Any) {
+        self.checkPassword()
+    }
+    
+    }
+
