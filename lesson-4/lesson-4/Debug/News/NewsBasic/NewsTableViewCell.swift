7f07c2d38bf1faae5e4ab9cb85cd5e6bb06addc2
LACKY5533 <33594361+LACKY5533@users.noreply.github.com>
2021-09-19 15:16:11 +0300

    lesson-4
    lesson-4

---
+++ lesson-4/lesson-4/Debug/News/NewsBasic/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  lesson-4
+//
+//  Created by LACKY on 19.09.2021.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
