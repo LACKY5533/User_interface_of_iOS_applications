97076b04c1c30451359e160db1374f7a1da0b60a
LACKY5533 <33594361+LACKY5533@users.noreply.github.com>
2021-09-25 18:30:18 +0300

    Add files via upload

---
+++ Пользовательский интерфейс iOS-приложений/lesson-6/lesson-6/Debug/Settings/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  lesson-5
+//
+//  Created by LACKY on 20.09.2021.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func MainSettingsButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func notificationsButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func blackListButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func ExitButton(_ sender: Any) {
+        
+    }
+    
+}
