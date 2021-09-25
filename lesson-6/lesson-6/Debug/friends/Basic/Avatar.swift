dbe43b45e0fd9b4e493a016166a694bad5d1cf83
LACKY5533 <33594361+LACKY5533@users.noreply.github.com>
2021-09-25 18:57:30 +0300

    lesson-6

---
+++ lesson-6/lesson-6/Debug/friends/Basic/Avatar.swift
@@ -0,0 +1,33 @@
+//
+//  Avatar.swift
+//  lesson-4
+//
+//  Created by LACKY on 20.09.2021.
+//
+
+import UIKit
+
+//@IBDesignable
+class Avatar: UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.Shadow()
+    }
+    
+    private func Shadow() {
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = .init(width: 5, height: -5)
+        self.layer.shadowOpacity = 0.15
+    }
+    
+}
