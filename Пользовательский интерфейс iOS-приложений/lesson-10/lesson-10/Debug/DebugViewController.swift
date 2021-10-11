a94d997bf0000542629719c899caef1f1cc78254
LACKY5533 <33594361+LACKY5533@users.noreply.github.com>
2021-10-11 13:18:16 +0300

    Add files via upload

---
+++ Пользовательский интерфейс iOS-приложений/lesson-10/lesson-10/Debug/DebugViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DebugViewController.swift
+//  lesson-3
+//
+//  Created by LACKY on 10.09.2021.
+//
+
+import UIKit
+
+class DebugViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
